0cc8383b8ed6c7f5f727a0cd4bf22d972c3eaa20
MindHawk <timgooren@hotmail.nl>
2021-10-07 13:16:12 +0200

    Initial commit

---
+++ GGCGatewayTest/SampleTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GGCGatewayTest
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void SampleMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
