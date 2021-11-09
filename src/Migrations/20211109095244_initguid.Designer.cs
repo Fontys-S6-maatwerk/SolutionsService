72e92bed7fd82fae307b1938597e645e7b778bb4
EGKangaroo <EGKangaroo@yahoo.com>
2021-11-09 10:53:21 +0100

    get rekt, don't use ensurecreated in the context class

--- src/Migrations/20211109095244_initguid.Designer.cs
+++ src/Migrations/20211109095244_initguid.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SolutionsService.Data;
 
 namespace SolutionsService.Migrations
 {
     [DbContext(typeof(SolutionsServiceContext))]
-    partial class SolutionsServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20211109095244_initguid")]
+    partial class initguid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
