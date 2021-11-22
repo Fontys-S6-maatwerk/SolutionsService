14f90b6ffa6c6bc1a4e62ca76cffec075ba7e46c
EGKangaroo <EGKangaroo@yahoo.com>
2021-11-22 13:40:30 +0100

    Test Coverage

---
+++ test/Models/SDGSolutionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SolutionsService.Models;
+
+namespace SolutionsTest.Models
+{
+    public class SDGSolutionTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void PropertiesTest()
+        {
+            Guid SolId = Guid.NewGuid();
+            Guid SDGId = Guid.NewGuid();
+
+            SDGSolution sdgsol = new SDGSolution()
+            {
+                SolutionId = SolId,
+                SDGId = SDGId,
+                Solution = new Article() { Id = SolId },
+                SDG = new SDG() { Id = SDGId }
+            };
+
+            Assert.AreEqual(SDGId, sdgsol.SDGId);
+            Assert.AreEqual(SolId, sdgsol.SolutionId);
+
+            Assert.NotNull(sdgsol.Solution);
+            Assert.NotNull(sdgsol.SDG);
+
+            Assert.AreEqual(sdgsol.SDG.Id, sdgsol.SDGId);
+            Assert.AreEqual(sdgsol.Solution.Id, sdgsol.SolutionId);
+        }
+    }
+}
