fa83e30eed25e1d7083e5bf3d3fc7f1bb0912545
Tim Gooren <384376@student.fontys.nl>
2021-11-25 13:50:45 +0100

    Create response models

---
+++ src/Models/ResponseModels/SDGResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolutionsService.Models.ResponseModels
+{
+    public class SDGResponse
+    {
+        public string Name { get; set; }
+        public int SDGNumber { get; set; }
+    }
+}
