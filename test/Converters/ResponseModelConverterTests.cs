11b85d2b4d22ed745c19496c7ed3791d16de45e2
EGKangaroo <EGKangaroo@yahoo.com>
2021-12-02 10:54:04 +0100

    sdg response conversion test

---
+++ test/Converters/ResponseModelConverterTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using SolutionsService.Models;
+using SolutionsService.Models.ResponseModels;
+using SolutionsService.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolutionsService.Converters;
+
+namespace SolutionsTest.Converters
+{
+    public class ResponseModelConverterTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void PopulatedSDGTest()
+        {
+            SDG sdg = new SDG()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Honger",
+                SDGNumber = 1
+            };
+
+            SDGResponse response = ResponseModelBuilder.BuildSDGResponse(sdg);
+
+            Assert.AreEqual("Honger", response.Name);
+            Assert.AreEqual(1, response.SDGNumber);
+            Assert.AreEqual(sdg.Id, response.Id);
+        }
+    }
+}
